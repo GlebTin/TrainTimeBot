93e05fdae69147b38a173c29044f6368a843912c
GlebBolotin <bolotinn5@mail.ru>
2024-03-03 23:24:20 +0300

    changes

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types
+
 
 bot = telebot.TeleBot('7064548980:AAEJa6IwjBbooyf0UBMyOwJS0ieTLzjnwlk')
 
@@ -7,7 +9,8 @@ name = ''
 surname = ''
 age = ''
 
-@bot.message_handler(func=lambda message: True) #content_types=['text'])
+
+@bot.message_handler(func=lambda message:True)
 def start(message):
     if message.text == '/reg':
         bot.send_message(message.from_user.id, "Hello!\n"
@@ -28,18 +31,28 @@ def get_surname(message):
     global surname
     surname = message.text
     bot.send_message(message.from_user.id, "How old are you?")
-    bot.register_message_handler(message, get_age)
+    bot.register_next_step_handler(message, get_age)
 
 
 def get_age(message):
     global age
-    while age == 0:
-        try:
-            age = int(message.text)
-        except Exception:
-            bot.send_message(message.from_user.id, 'Please enter the value in digits.')
-    bot.send_message(message.from_user.id, 'Your age is ' + str(age) + ', your name is '
-                         + name + surname + '?')
+    try:
+        age = int(message.text)
+        keyboard = types.InlineKeyboardMarkup()
+        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+        key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+        keyboard.add(key_yes, key_no)
+        question = f'''You`re {str(age)}years old and your name is {name} {surname}?'''
+        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    except ValueError:
+        bot.send_message(message.from_user.id, 'Please enter the value in digits.')
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_worker(call):
+        if call.data == 'yes':
+            bot.send_message(message.from_user.id, 'I got it!')
+        elif call.data == 'no':
+            bot.send_message(message.from_user.id, 'Please repeat, write /reg')
 
 
 bot.polling(none_stop=True, interval=0)
